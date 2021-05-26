758ba69ef60b9940021d89433e491b5ebf0de7f6
kevin-valencia <80435646+kevin-valencia@users.noreply.github.com>
2021-05-26 16:37:00 -0500

    Add files via upload

---
+++ src/src/main.java
@@ -0,0 +1,14 @@
+import Entidades.colores;
+import Entidades.Yate;
+import Entidades.Carro;
+import Entidades.Avion;
+import Utilidades.Menu;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class main {
+
+
+    public static void main(String[] args) {
+        new Menu().correrMenu();
+    }
+}
